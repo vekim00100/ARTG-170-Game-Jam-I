37ac92f993234d429b94036152576e49eb2255d6
vekim00100 <vekim@ucsc.edu>
2025-11-06 07:27:17 -0800

    Integrate door interaction

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,9 +1,22 @@
-extends Node3D
+extends Area3D
 
-func _ready():
-	$AnimationPlayer.play("door_open")
-	$AnimationPlayer.animation_finished.connect(_on_animation_finished)
+@export var text_key := "door_interact"
+var nearDoor := false
 
-func _on_animation_finished(anim_name):
-	if anim_name == "door_open":
-		get_tree().change_scene_to_file("res://scenes/main.tscn")
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if nearDoor and Input.is_action_just_pressed("Context_Interact"):
+		# Change to the new scene when player presses F
+		get_tree().change_scene_to_file("res://scenes/doorscene.tscn")
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.name == "Debug Player":
+		nearDoor = true
+		SignalBus.player_interaction_available.emit(text_key)
+		print("Player entered door area.")
+
+func _on_body_exited(body: Node3D) -> void:
+	if body.name == "Debug Player":
+		nearDoor = false
+		SignalBus.player_interaction_available.emit(null)
+		print("Player left door area.")
